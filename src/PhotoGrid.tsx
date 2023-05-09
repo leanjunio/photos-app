d4a2cedecc2c2e33a755eabe39c764d4acc53f8d
Lean Junio <leanjunio@outlook.com>
2023-05-09 12:43:08 -0400

    feat: add empty message

--- src/PhotoGrid.tsx
+++ src/PhotoGrid.tsx
@@ -11,17 +11,19 @@ export function PhotoGrid({ currentList, onPhotoSelect }: PhotoGridProps) {
   const current = usePhotosStore(state => state.current);
   return (
     <div aria-label="photo grid" className="content">
-      <div className="photo-grid">
-        {currentList.map((photo) => (
-          <div key={photo.id} className="photo-grid__wrapper" onClick={() => onPhotoSelect(photo.id)}>
-            <img className={`photo-grid__img ${current === photo.id ? 'photo-grid__img--selected' : ''}`} src={photo.url} alt={photo.filename} />
-            <FileDetails
-              filename={photo.filename}
-              size={bytesToMB(photo.sizeInBytes)}
-            />
-          </div>
-        ))}
-      </div>
+      {currentList.length === 0 ? <p>No photos found</p> : (
+        <div className="photo-grid">
+          {currentList.map((photo) => (
+            <div key={photo.id} className="photo-grid__wrapper" onClick={() => onPhotoSelect(photo.id)}>
+              <img className={`photo-grid__img ${current === photo.id ? 'photo-grid__img--selected' : ''}`} src={photo.url} alt={photo.filename} />
+              <FileDetails
+                filename={photo.filename}
+                size={bytesToMB(photo.sizeInBytes)}
+              />
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
